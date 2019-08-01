7e676f01ff492984d641321cba3e544ef91fd84e
DrMaxis <Grandmasterantwii@gmail.com>
2019-08-01 19:31:14 -0400

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title', app_name())</title>
+        <meta name="description" content="@yield('meta_description', 'Brown Paper Bag')">
+        <meta name="author" content="@yield('meta_author', 'Nathan Antwi')">
+        @yield('meta')
 
     <style>
     
